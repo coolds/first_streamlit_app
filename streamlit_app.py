7b40dcbac4595aae638a99582e23ed7070fb7de7
coolds <33264080+coolds@users.noreply.github.com>
2023-03-08 12:51:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ streamlit.dataframe(my_data_rows)
 streamlit.write('Thnks for adding ', add_my_fruit)
 
 #This will not work correctly but just go with it for now
-my_cur.execute("insert into fuit_load_list_values values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
